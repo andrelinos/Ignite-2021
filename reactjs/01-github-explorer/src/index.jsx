0b430c653cf151461d4df3048e399349642d1074
Andrelino Silva <andrelinocom@gmail.com>
2021-03-15 21:01:08 -0300

    Servindo HTML estático

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return <h1>Hello Word</h1>
-};
+import { App } from './App';
+
+render(<App />, document.getElementById('root'));

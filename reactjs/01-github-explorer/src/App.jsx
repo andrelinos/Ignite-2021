9cf9c8bd7746bcf5abd94abd8917e25c776201d0
Andrelino Silva <andrelinocom@gmail.com>
2021-03-17 07:58:09 -0300

    Criando components e props

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/gobal.scss';
 
 export function App() {
-  return <h1>Hello World</h1>
+  return <RepositoryList />
 };
---
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository?.name ?? 'Default'}</strong>
+      <p>{props.repository?.description ?? 'Default'}</p>
+
+      <a href={props.repository?.link ?? 'Default'}>
+        Acessar repositório
+          </a>
+    </li>
+  );
+}

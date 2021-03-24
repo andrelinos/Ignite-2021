84080a2d3597e23914ecfd235a32427ddde4d3b1
Andrelino Silva <andrelinocom@gmail.com>
2021-03-24 08:27:24 -0300

    Configurado TypeScript para o projeto

--- reactjs/01-github-explorer/babel.config.js
+++ reactjs/01-github-explorer/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    '@babel/preset-typescript',
     ['@babel/preset-react', {
       runtime: 'automatic' /* Evita ter que importar o React em todo arquivo */
     }],

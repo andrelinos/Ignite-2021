ca74ccc4e5003b60ef426c23206f702d0a086de4
Andrelino Silva <andrelinocom@gmail.com>
2021-04-06 07:52:53 -0300

    Add react-modal

--- reactjs/dtmoney/src/components/Header/index.tsx
+++ reactjs/dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handelOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handelCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
-      <img src={logo} alt="dt money"/>
-      <button type="button">
-        Nova transação
+        <img src={logo} alt="dt money" />
+        <button type="button" onClick={handelOpenNewTransactionModal}>
+          Nova transação
       </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handelCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       </Content>
     </Container>
   )

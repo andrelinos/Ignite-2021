8e0473841707df6b1329abde58352729ff262e27
Andrelino Silva <andrelinocom@gmail.com>
2021-04-11 07:47:11 -0300

    Adicionando useContext

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -4,6 +4,8 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
+
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handelOpenNewTransactionModal} />
 
       <Dashboard />
@@ -30,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }

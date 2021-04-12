8bae27f380ef069a3a56061c1813348aa417315d
Andrelino Silva <andrelinocom@gmail.com>
2021-04-12 07:24:41 -0300

    Inserindo itens na lista

--- reactjs/dtmoney/src/TransactionsContext.tsx
+++ reactjs/dtmoney/src/TransactionsContext.tsx
@@ -18,12 +18,12 @@ interface TransactionProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
-  );
+);
 
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -33,8 +33,17 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       .then(response => setTransactions(response.data.transactions));
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('transaction', transaction);
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction
+    ]);
   }
 
   return (

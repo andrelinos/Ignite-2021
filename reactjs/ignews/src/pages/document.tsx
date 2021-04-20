75fb20bd616df73b36500a459fd3673c73f9caf7
Andrelino Silva <andrelinocom@gmail.com>
2021-04-20 20:05:23 -0300

    Resolvendo erro Head

--- reactjs/ignews/src/pages/document.tsx
+++ reactjs/ignews/src/pages/document.tsx
@@ -1,12 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+import '../styles/global.scss';
+
 export default class MyDocument extends Document {
   reder() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
         

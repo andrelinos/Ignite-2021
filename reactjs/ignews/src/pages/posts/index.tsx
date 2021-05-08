b53c449e1ecd19422ba2887020db665423761e4f
Andrelino Silva <andrelinocom@gmail.com>
2021-05-08 08:18:02 -0300

    Add Prismic e Buscando dados da API

--- reactjs/ignews/src/pages/posts/index.tsx
+++ reactjs/ignews/src/pages/posts/index.tsx
@@ -1,4 +1,8 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -15,28 +19,25 @@ export default function Posts() {
             <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages a shared...</p>
           </a>
-          <a href="#">
-            <time>08 maio de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages a shared...</p>
-          </a>
-          <a href="#">
-            <time>08 maio de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages a shared...</p>
-          </a>
-          <a href="#">
-            <time>08 maio de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages a shared...</p>
-          </a>
-          <a href="#">
-            <time>08 maio de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages a shared...</p>
-          </a>
         </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  // console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {}
+  };
 }

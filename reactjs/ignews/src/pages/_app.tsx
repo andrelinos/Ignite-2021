5055e5b1208803d07413f9fccfe86985c4be7e5a
Andrelino Silva <andrelinocom@gmail.com>
2021-05-16 09:01:04 -0300

    Update destalhes

--- reactjs/ignews/src/pages/_app.tsx
+++ reactjs/ignews/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
-import {Provider as NextAuthProvider} from 'next-auth/client';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import '../styles/global.scss';
 

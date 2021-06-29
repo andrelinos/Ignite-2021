5594c13584641e76eaa1b30c2d61d054f165931a
Andrelino Silva <andrelinocom@gmail.com>
2021-06-29 07:49:49 -0300

    Definindo staleTime

--- reactjs/dashgo/src/pages/users/index.tsx
+++ reactjs/dashgo/src/pages/users/index.tsx
@@ -36,6 +36,8 @@ export default function UserList() {
       const response = await api.get(`users/${userId}`);
 
       return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10, // 10 minutos
     });
   }
 

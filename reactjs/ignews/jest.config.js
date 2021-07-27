f7d035649b50a9cb71788b14ec4c3bab31205f69
Andrelino Silva <andrelinocom@gmail.com>
2021-07-27 08:13:49 -0300

    Criando tests

---
+++ reactjs/ignews/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/.vscode/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+      },
+  testEnvironment: "jsdom",
+};
+

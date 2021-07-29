0a68e1f5781b812f64e4d80a59e6d09f510251b5
Andrelino Silva <andrelinocom@gmail.com>
2021-07-29 07:46:52 -0300

    Criando teste do SignInButton

---
+++ reactjs/ignews/src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
+import { useSession } from 'next-auth/client';
+import { SignInButton } from '.';
+
+jest.mock('next-auth/client');
+
+describe('SignInButton component', () => {
+  const useSessionMocked = mocked(useSession);
+
+  it('render correctly when user is not autheticated', () => {
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument;
+  });
+
+  it('render correctly when user is autheticated', () => {
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: { name: 'John Doe', email: 'john.doe@example.com' },
+        expires: 'fake-expires'
+      },
+      false
+    ]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText('John Doe')).toBeInTheDocument;
+  });
+});
